1d0e754a2ef0c829e7c34788794249fdf07dd75d
varshachopra22 <varsha.puri@gmail.com>
2019-07-07 19:38:44 +1000

    Create Test Added

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyFirstApp
@@ -17,6 +18,7 @@ namespace MyFirstApp
 
             // launch turnup portal
             driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2fTimeMaterial");
+            driver.Manage().Window.Maximize();
 
             //Enter username
             IWebElement Username = driver.FindElement(By.Id("UserName"));
@@ -33,7 +35,7 @@ namespace MyFirstApp
             //validate if user has logged in successfully
             IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
 
-            if(hellohari.Text=="hello hari")
+            if (hellohari.Text == "Hello hari!")
             {
                 Console.WriteLine("Logged in successfully. Test has passed");
             }
@@ -41,36 +43,53 @@ namespace MyFirstApp
             {
                 Console.WriteLine("Test has failed");
             }
-            // Navigate to Times and material
+            //Navigate to Times and material
             //Click on Administration menu
-            driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a")).Click();
+            IWebElement Adm = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            Adm.Click();
 
             //click on Times and materials
-            driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a")).Click();
+            IWebElement TimeMate = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            TimeMate.Click();
 
             //click on Create button
-            driver.FindElement(By.XPath("//*[@id='container']/p/a")).Click();
+            IWebElement Createbut = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            Createbut.Click();
 
             //Select the value from Typecode dropdown
-            driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]")).Click();
-            driver.FindElement(By.XPath(//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[2]")).Click();
+            IWebElement TypeCode = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+            TypeCode.Click();
 
             //Enter value for Code
-            driver.FindElement(By.XPath();
+            IWebElement CodeTxt = driver.FindElement(By.XPath("//*[@id='Code']"));
+            CodeTxt.SendKeys("AutoCode");
 
             //Enter value Descrition
-            driver.FindElement(By.Id("Description").SendKeys("autodescp"));
+            IWebElement Desc = driver.FindElement(By.Id("Description"));
+            Desc.SendKeys("autodescp");
 
             //Enter Price per unit
-            driver.FindElement(By.XPath("Price").SendKeys(4500);
+            //IWebElement Priceextbox = driver.FindElement(By.Id("Price"));
+            //Priceextbox.SendKeys(String.valueOf(4500));
 
             //Click on Save button
             driver.FindElement(By.XPath("//*[@id='SaveButton']")).Click();
+            Thread.Sleep(1000);
 
             //Navigate to the last page
+            IWebElement lastpg = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[2]/td[1]"));
+            lastpg.Click();
 
             //verify the existence of the record
-
+            IWebElement NewRec = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[2]/td[1]"));
+            if(NewRec.Text == "Autocode")
+            {
+                Console.WriteLine("Record successfully created");
+            }
+            else
+            {
+                Console.WriteLine("Record not created");
+            }
         }
     }
 }
